fd31dba0ccef331a682ecb339326bece7c0a237d
COOL- <87934102+msajo@users.noreply.github.com>
2023-02-16 17:36:48 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -9,13 +9,17 @@ Webcam.attach("#camera");
 
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri +'">';
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri +'">';
     });
 }
 
 console.log("ml5 version",ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/9pJp0hVOV/.json",modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/9pJp0hVOV/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Loaded");
+}
 
 function check(){
     img = document.getElementById("capture_image");
@@ -24,7 +28,7 @@ function check(){
 
 function gotresult(error,results){
     if(error){
-        console.log("error");
+        console.error(error);
     }
     else{
         console.log("results");
